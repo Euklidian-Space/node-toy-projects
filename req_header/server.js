f6248bb2965049cd89f4d0c6b4413a6e23acbb69
Gabriel <gabriel.grover@aofl.com>
2017-09-26 12:08:27 -0700

    req-header microservice finished

--- req_header/server.js
+++ req_header/server.js
@@ -4,22 +4,17 @@ const useragent = require('useragent');
 
 const app = express();
 
-// app.use('/scripts', express.static(__dirname + '/node_modules/clientjs/dist/'));
-
-// app.get("/", (req, res) => {
-// 	let fileName = path.join(__dirname, "static/index.html");
-// 	res.sendFile(fileName, err => {
-// 		if (err)
-// 			res.status(err.status).end();
-// 		else
-// 			console.log("Sent: ", fileName);
-// 	});
-// });
-
 app.get("/", (req, res) => {
 	let agent = useragent.parse(req.headers["user-agent"]);
 	let ip = req.ip;
 	let software = agent.os.toString();
+	let lang = req.headers["accept-language"].split(",")[0];
+
+	res.json({
+		ip,
+		lang,
+		software
+	});
 });
 
 
